bc75a8d0b68b094af3e0653f2aad22a61d5253d4
proall <allaboutpro22@gmail.com>
2026-02-23 12:11:46 -0500

    dispatch detils in favorite slice

--- src/redux/favoritesSlice.js
+++ src/redux/favoritesSlice.js
@@ -1,16 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
-
 const initialState = {
   favoriterecipes: [], // Updated to handle favorite articles
 };
-
 const favoritesSlice = createSlice({
   name: "favorites",
   initialState,
   reducers: {
-   
+    toggleFavorite: (state, action) => {
+      const recipe = action.payload;
+      const index = state.favoriterecipes.findIndex(
+        (fav) => fav.idFood === recipe.idFood
+      );
+      if (index !== -1) {
+        state.favoriterecipes.splice(index, 1);
+      } else {
+        state.favoriterecipes.push(recipe);
+      }
+    },
   },
 });
-
 export const { toggleFavorite } = favoritesSlice.actions;
 export default favoritesSlice.reducer;
